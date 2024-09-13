fb1407c3c1b1736de67dd12d256a64abdb451151
Rocksound_sultan <123336043+Rocksound007@users.noreply.github.com>
2024-09-13 17:02:43 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(20), nullable=False)  # "student" or "teacher"
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Lesson(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
